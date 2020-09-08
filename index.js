9640aebd6451991b409b99dc0cdf96a58412dca5
Gklam <khalid.lamsadi@outlook.fr>
2020-09-08 17:25:03 +0100

    Add Image Modal feature

--- index.js
+++ index.js
@@ -40,3 +40,32 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
+
+/* -----------------------------------------
+  Handling Image Model
+ ---------------------------------------- */
+
+document.querySelectorAll(".work__image").forEach((img) => {
+  img.addEventListener("click", (e) => {
+    // Get the modal
+    var modal = document.getElementById("imgModel");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = e.target;
+    var modalImg = document.getElementById("modal-img");
+    var captionText = document.getElementById("caption");
+    img.onclick = function () {
+      modal.style.display = "block";
+      modalImg.src = this.dataset.src;
+      captionText.innerHTML = this.alt;
+    };
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("modal-close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+  });
+});
